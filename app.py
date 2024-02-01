303194efc1d97fcc09b48feb865393cd68bf5fd7
TanishSinghTak <tanishsingh.tak9829@gmail.com>
2024-02-01 18:24:06 +0530

    Added comments to app.py

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
+#Importing Libraries
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.util import method_is_overridden
 from werkzeug.security import generate_password_hash, check_password_hash
 import yfinance as yf
 import pandas as pd
-import json
 
+# Flask Configuration
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Replace with your actual secret key
+
+# Reading Data of stocks from csv files
 Tickers_df = pd.read_csv("tickers_data.csv", index_col=0)
-Tickers_dict = Tickers_df.to_dict(orient='index')
+Tickers_dict = Tickers_df.to_dict(orient='index') # Dictionary to store data of all stocks
 Symbols_df = pd.read_csv('symbols.csv')
-symbolsList = Symbols_df['Symbol'].tolist()
+symbolsList = Symbols_df['Symbol'].tolist() # List of all stock symbols of NSE
 symbolsList.insert(0, '')
 
-# Database Configuration
+# Database Configuration (Stores user data)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -29,18 +32,20 @@ class User(db.Model):
 with app.app_context():
     db.create_all()
 
+# Index
 @app.route('/')
 def index():
     return render_template('login.html')
 
+# Register Route
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256') # Hashed password for security
 
-        new_user = User(username=username, password_hash=hashed_password)
+        new_user = User(username=username, password_hash=hashed_password) # Creating new user
         db.session.add(new_user)
         db.session.commit()
 
@@ -49,24 +54,27 @@ def register():
 
     return render_template('register.html')
 
+# Login Route
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
     user = User.query.filter_by(username=username).first()
 
-    if user and check_password_hash(user.password_hash, password):
+    if user and check_password_hash(user.password_hash, password): # Checking if user exists and password is correct
         session['user_id'] = user.id
         session['username'] = user.username
         return redirect(url_for('dashboard'))
     else:
-        flash('Invalid username or password')
+        flash('Invalid username or password') 
         return redirect(url_for('index'))
 
+# Dashboard Route
 @app.route('/dashboard', methods=['GET'])
 def dashboard():
     return render_template('dashboard.html', username=session['username'])
 
+# Data route to fetch data of a stock for chart
 @app.route('/data', methods=['POST'])
 def getdata():
     stock_name = request.json['stock_name']
@@ -76,8 +84,7 @@ def getdata():
 
     # Ensure data is sorted by time
     stk_data = stk_data.sort_index()
-    print(stk_data.columns)
-    # Formatting data for Chart.js financial chart
+    # Formatting data for HighChart.js financial chart
     ohlc_data = []
     for index, row in stk_data.iterrows():
         ohlc_data.append({
@@ -93,43 +100,43 @@ def getdata():
         'label': stock_name,
         'data': ohlc_data
     }
-    print("reqest processed")
     return jsonify(chart_data)
 
+# Route to fetch stock names according to filter
 @app.route('/stock_names', methods=['POST'])
 def get_stock_names():
-    # Fetch and return all available stock names
-    # You need to implement this based on your data source (e.g., database)
-    # For now, assume you have a list of stock names
-    filter_type = request.json['filter_type']
-    min_filter_value = float(request.json['min_filter_value'])
-    max_filter_value = float(request.json['max_filter_value'])
+    filter_type = request.json['filter_type'] # Type of filter
+    min_filter_value = float(request.json['min_filter_value']) # Minimum value of filter
+    max_filter_value = float(request.json['max_filter_value']) # Maximum value of filter
     stock_names = symbolsList
-    if filter_type == 'default' or (min_filter_value == 0.0 and max_filter_value == 0.0):
+
+    if filter_type == 'default' or (min_filter_value == 0.0 and max_filter_value == 0.0): # If no filter is applied
         pass
-    elif filter_type == 'trailingPE':
+    elif filter_type == 'trailingPE': # If filter is applied on trailing P/E ratio
         stock_names = [symbol for symbol, info in Tickers_dict.items() if
                     info.get('trailingPE', 0.0) > min_filter_value and
                     info.get('trailingPE', 0.0) < max_filter_value]
-    elif filter_type == 'forwardPE':
+    elif filter_type == 'forwardPE': # If filter is applied on forward P/E ratio
         stock_names = [symbol for symbol, info in Tickers_dict.items() if
                     info.get('forwardPE', 0.0) > min_filter_value and
                     info.get('forwardPE', 0.0) < max_filter_value]
-    elif filter_type == 'fiftyDayAverage':
+    elif filter_type == 'fiftyDayAverage': # If filter is applied on 50 day average
         stock_names = [symbol for symbol, info in Tickers_dict.items() if
                     info.get('fiftyDayAverage', 0.0) > min_filter_value and
                     info.get('fiftyDayAverage', 0.0) < max_filter_value]
-    elif filter_type == 'twoHundredDayAverage':
+    elif filter_type == 'twoHundredDayAverage': # If filter is applied on 200 day average
         stock_names = [symbol for symbol, info in Tickers_dict.items() if
                     info.get('twoHundredDayAverage', 0.0) > min_filter_value and
                     info.get('twoHundredDayAverage', 0.0) < max_filter_value]
     return jsonify({'stock_names': stock_names})
 
+# Route to fetch stock info for a particular stock
 @app.route('/stock_info', methods=['POST'])
 def get_stock_info():
     stock_name = request.json['stock_name']
-    stock_info = yf.Ticker(stock_name).info
-    print(stock_info)
+    stock_info = yf.Ticker(stock_name).info # Fetching stock info from Yahoo Finance API to a dictionary
+
+    # Extracting required data from dictionary
     dayLow = stock_info.get('dayLow', '-')
     dayHigh = stock_info.get('dayHigh', '-')
     yearLow = stock_info.get('fiftyTwoWeekLow', '-')
@@ -153,23 +160,26 @@ def get_stock_info():
     if(totalDebt != '-'):
         totalDebt = toCr(totalDebt)
     about = stock_info['longBusinessSummary']
+
     return jsonify({'dayLow': dayLow, 'dayHigh': dayHigh, 'yearLow': yearLow, 'yearHigh': yearHigh, 'Open': Open, 'previousClose': previousClose, 'volume': volume,
                     'currentPrice': currentPrice, 'marketCap': marketCap, 'trailingPE': trailingPE, 'forwardPE': forwardPE, 'fiftyDayAverage': fiftyDayAverage,
                     'twoHundredDayAverage': twoHundredDayAverage, 'dividendYield': dividendYield, 'totalRevenue': totalRevenue, 'totalDebt': totalDebt, 'about': about
                     })
 
+# Logout Route 
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
     session.pop('username', None)
     return redirect(url_for('index'))
 
-
+# Function to convert Huge numbers to Cr unit
 def toCr(num):
     num = num/10000000
     num = round(num, 2)
     num = str(num) + 'Cr'
     return num
 
+# Main Function
 if __name__ == '__main__':
     app.run(debug=True)
